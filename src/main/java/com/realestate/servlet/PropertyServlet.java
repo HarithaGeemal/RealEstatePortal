9022a7d858e259c047767930f79b031b83b73a87
Haritha Geemal <128626728+HarithaGeemal@users.noreply.github.com>
2025-05-17 00:27:19 +0530

    Initial commit of RealEstatePortal project updated

--- src/main/java/com/realestate/servlet/PropertyServlet.java
+++ src/main/java/com/realestate/servlet/PropertyServlet.java
@@ -29,7 +29,7 @@ public class PropertyServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    rotected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String action = req.getParameter("action");
         String id = req.getParameter("id");
 
